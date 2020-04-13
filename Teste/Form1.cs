f6b3bdb8082c3bf7b0f050ce67577bceee778ff6
Dener <denercerantola3@hotmail.com>
2020-04-13 17:46:39 -0300

    Codigo e Exemplo

---
+++ Teste/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DesignDiferente();
+        }
+        private void DesignDiferente()
+        {
+            pnlEsconde.Visible = false;
+        }
+        private void Hide()
+        {
+            if (pnlEsconde.Visible == true)
+                pnlEsconde.Visible = false;
+        }
+        private void Show(Panel pnl)
+        {
+            if (pnlEsconde.Visible == false)
+            {
+                Hide();
+                pnlEsconde.Visible = true;
+            }
+            else
+            {
+                pnlEsconde.Visible = false;
+            }
+               
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            Show(pnlEsconde);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Deu bom!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Deu bom!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Deu bom!");
+        }
+    }
+}
